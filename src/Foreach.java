35392e37a8fd520e36a17d99a0860f312ade9657
MandS <monika.domaradzkaa@gmail.com>
2022-11-23 18:24:04 +0000

    Uzycie foreach

---
+++ src/Foreach.java
@@ -0,0 +1,8 @@
+public class Foreach {
+    public static void main(String[] args) {
+        String[] cats=new String[]{"Britney","Gloria","Iqos","Pussy"};
+        for (String cat:cats){
+            System.out.println(cat);
+        }
+    }
+}
